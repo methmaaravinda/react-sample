056b79d860241d0e44c256899051c037722bf14c
methamaaravinda <methmaaravinda@gmail.com>
2022-12-23 17:13:05 +0530

    new

--- src/components/Hemal.js
+++ src/components/Hemal.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Hemal() {
   return (
     <div>
-      hello hemal
+      hemal
     </div>
   )
 }
--- src/components/Prasanna.js
+++ src/components/Prasanna.js
@@ -3,8 +3,7 @@ import React from 'react'
 export default function Prasanna() {
   return (
     <div>
-      my self 
-     i am Dilshan Aravinda, i am from gampaha. I studied in Civil.
+      prasanna
     </div>
   )
 }
